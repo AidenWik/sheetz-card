66b78e998680573dc51e4deb1228000e29855ddb
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-01 13:58:05 -0400

    as

--- src/sheetz-card.js
+++ src/sheetz-card.js
@@ -228,7 +228,75 @@ class SheetzCard extends LitElement {
        </div>
        
       
-   
+      <script> 
+      // JavaScript to toggle visibility of the paragraph descriptions
+const toggleDetailsButtons = document.querySelector(sheetz-card).shadowRoot.querySelectorAll('.toggle-details');
+const paragraphsSections = document.querySelector(sheetz-card).shadowRoot.querySelectorAll('.paragraphs');
+
+toggleDetailsButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    paragraphsSections[index].classList.toggle('hidden');
+  });
+});
+
+// JavaScript for cloning cards
+document.querySelector(sheetz-card)shadowRoot.querySelector('#duper').addEventListener('click', (e) => {
+  const cardContainer = document.querySelector(sheetz-card).shadowRoot.querySelector('.card-container');
+  const cardWrapperClone = document.querySelector(sheetz-card).shadowRoot.querySelector('.card-wrapper').cloneNode(true);
+
+  // Add unique classes to the cloned elements
+  cardWrapperClone.classList.add('cloned-card');
+  cardWrapperClone.querySelector('.toggle-details').textContent = 'Details';
+
+  cardContainer.appendChild(cardWrapperClone);
+
+  // Set up event listeners for the cloned button
+  const clonedToggleDetailsButton = cardWrapperClone.querySelector('.toggle-details');
+  const clonedParagraphsSection = cardWrapperClone.querySelector('.paragraphs');
+
+  clonedToggleDetailsButton.addEventListener('click', () => {
+    clonedParagraphsSection.classList.toggle('hidden');
+  });
+});
+document.querySelector('#deleteLastCard').addEventListener('click', (e) => {
+// Code for deleting the last card
+const cardContainer = document.querySelector(sheetz-card).shadowRoot.querySelector('.card-container');
+const cardWrappers = document.querySelector(sheetz-card).shadowRoot.querySelectorAll('.card-wrapper');
+
+// Check if there's at least one card to delete
+if (cardWrappers.length > 1) {
+  const lastCard = cardWrappers[cardWrappers.length - 1];
+  cardContainer.removeChild(lastCard);
+}
+});
+
+document.querySelector(sheetz-card).shadowRoot.querySelector('.card-wrapper').addEventListener('mouseover', (e) => {
+const cardWrapper = document.querySelector(sheetz-card).shadowRoot.querySelectorAll('.card-wrapper');
+cardWrapper.classList.toggle('alt-bg2');
+});
+
+document.querySelector(sheetz-card).shadowRoot.querySelector('.card-wrapper').addEventListener('mouseleave', (e) => {
+const cardWrapper = document.querySelector(sheetz-card).shadowRoot.querySelectorAll('.card-wrapper');
+cardWrapper.classList.toggle('alt-bg2');
+});
+
+document.querySelector(sheetz-card).shadowRoot.querySelector('#title').addEventListener('click', (e) => {
+const headings = document.querySelector(sheetz-card).shadowRoot.querySelectorAll('.sheetzLbl'); // Corrected class name
+headings.forEach((heading) => {
+heading.textContent = "something else";
+});
+});
+
+
+
+document.querySelector(sheetz-card).shadowRoot.querySelector('#background').addEventListener('click', (e) => {
+const cardWrappers = document.querySelector(sheetz-card).shadowRoot.querySelectorAll('.card-wrapper');
+/* loop to change each existing iteration*/
+cardWrappers.forEach((cardWrapper) => {
+cardWrapper.classList.toggle('alt-bg');
+});
+});
+      </script>
    
      </body>
     `;
