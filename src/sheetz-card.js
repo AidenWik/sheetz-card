48b22bbdb9319196a5522654e84ffd02042305b4
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-03 17:02:12 -0400

    asd

--- src/sheetz-card.js
+++ src/sheetz-card.js
@@ -186,11 +186,11 @@ class SheetzCard extends LitElement {
   detailsToggle(){
     // JavaScript to toggle visibility of the paragraph descriptions
 // const details = document.querySelector('sheetz-card').shadowRoot.querySelector('.toggle-details');
-const toggleDetailsButtons = document.querySelectorAll('.detailsBtn');
+const toggleDetailsButtons = document.querySelector('sheetz-card').shadowRoot.querySelectorAll('.detailsBtn');
 console.log(toggleDetailsButtons);
 toggleDetailsButtons.forEach((detailsBtn, index) => {
   console.log(toggleDetailsButton);
-const paragraphsSections = document.querySelectorAll('.paragraphs');
+const paragraphsSections = document.querySelector('sheetz-card').shadowRoot.querySelectorAll('.paragraphs');
     paragraphsSections[index].classList.toggle('hidden');
   });
 }
