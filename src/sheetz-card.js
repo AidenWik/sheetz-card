ebd9e60c80d66884181da346abd70474b6dc0882
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-03 12:22:19 -0400

    details toggle

--- src/sheetz-card.js
+++ src/sheetz-card.js
@@ -163,69 +163,47 @@ color: blue;
   <div class="Sheetz">
     <label class="sheetzLbl">Sheetz</label>
   </div>
-  
 <div class="directions">
   <label id="directionsLbl">Directions</label>
 </div>
-  
 <div class="Favorites">
   <label id="Favoriteslbl">Favorites</label>
   </div>
-  
 <div class="btn-wrapper">
   <button>Hotdog</button>
   <button>Fries</button>
   <button>Shake</button>
 </div>
-
 <div class="images">
   <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Hot_dog_with_mustard.png" alt="A beautiful landscape">
 
   <img src="https://media.istockphoto.com/id/496094410/photo/potatoes-fries-in-a-red-carton-box-isolated-fast-food.jpg?s=612x612&w=0&k=20&c=d7WnO6AW0C3A-xMSBa3Yb5rmSNelyY-jmxhW2efZq2Q=">
-
   <img 
 src="https://gofericecream.com/wp-content/uploads/2021/06/shake-.png"> 
-  
 </div>
-
  <div class="detailsBtn">
         <button class="toggle-details">Details</button>
     </div>
-     
-  
 <div class="paragraphs">
   <p1>$1.99 <br>A food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun.</p1>
   <p2>$0.99 <br>Side dish or snack typically made from deep-fried potatoes that have been cut into various shapes, especially thin strips.</p2>
   <p3>$5.99 <br> A sweet beverage made by blending milk, ice cream, and flavorings or sweeteners.</p3>
   </div>
-
- 
   </div>
-
   </div>
-  
   <div class ="duper">
    <button id="duper">Button for duplication</button>
     </div>   
-  
-  
-   
-  
   <div class ="background">
     <button id="background">Change Background</button>
   </div>
-   
-  
   <div class ="title">
-    <button id="title">Change Title</button>
+    <button id="title" @click="$(this.toggleDetails)">Change Title</button>
   </div>
-  
-
-  
     <div class="delete">
         <button id="deleteLastCard">Delete Last Card</button>
     </div>
-    
+
       <script> 
         /*
         //Chat GPTS idea
@@ -235,7 +213,7 @@ src="https://gofericecream.com/wp-content/uploads/2021/06/shake-.png">
     details.toggleAttribute('open');
   }
 });*/
-        
+  toggleDetails(){      
       // JavaScript to toggle visibility of the paragraph descriptions
 document.querySelector('.toggle-details').addEventListener('click', (e) => {
  // const details = document.querySelector('sheetz-card').shadowRoot.querySelector('.toggle-details');
@@ -246,6 +224,7 @@ const paragraphsSections = document.querySelector('sheetz-card').shadowRoot.quer
     paragraphsSections[index].classList.toggle('hidden');
 });
 })
+  }
 /*
 toggleDetailsButtons.forEach((button, index) => {#
 const toggleDetailsButtons = document.querySelector('sheetz-card').shadowRoot.querySelectorAll('.toggle-details');
