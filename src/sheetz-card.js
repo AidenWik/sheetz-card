6520d98df68f2c50e90db8d6edc27c1dfde1f9ac
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-03 16:35:09 -0400

    a

--- src/sheetz-card.js
+++ src/sheetz-card.js
@@ -154,10 +154,10 @@ class SheetzCard extends LitElement {
   
     cardContainer.appendChild(cardWrapperClone);
     // Set up event listeners for the cloned button
-    //const clonedToggleDetailsButton = cardWrapperClone.querySelector('.detailsBtn');
-    //const clonedParagraphsSection = cardWrapperClone.querySelector('.paragraphs');
+    const clonedToggleDetailsButton = cardWrapperClone.querySelector('.detailsBtn');
+    const clonedParagraphsSection = cardWrapperClone.querySelector('.paragraphs');
   
-      /*clonedParagraphsSection.classList.toggle('hidden');*/
+      clonedParagraphsSection.classList.toggle('hidden');
   }
   deleteCard(){
     // Code for deleting the last card
