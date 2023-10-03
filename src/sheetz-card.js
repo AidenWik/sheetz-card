e6643257a2f91afd1ecdea7de6c2bb8780774223
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-03 16:41:30 -0400

    aa

--- src/sheetz-card.js
+++ src/sheetz-card.js
@@ -157,7 +157,7 @@ class SheetzCard extends LitElement {
     const clonedToggleDetailsButton = cardWrapperClone.querySelector('.detailsBtn');
     const clonedParagraphsSection = cardWrapperClone.querySelector('.paragraphs');
   
-     // clonedParagraphsSection.classList.toggle('hidden');
+     //clonedParagraphsSection.classList.toggle('hidden');
   }
   deleteCard(){
     // Code for deleting the last card
@@ -186,7 +186,7 @@ class SheetzCard extends LitElement {
   detailsToggle(){
     // JavaScript to toggle visibility of the paragraph descriptions
 // const details = document.querySelector('sheetz-card').shadowRoot.querySelector('.toggle-details');
-const toggleDetailsButtons = document.querySelector('sheetz-card').shadowRoot.querySelectorAll('.detailsBtn');
+const toggleDetailsButtons = document.querySelector('sheetz-card').shadowRoot.querySelectorAll('#detailsBtn');
 const paragraphsSections = document.querySelector('sheetz-card').shadowRoot.querySelector('.paragraphs');
   toggleDetailsButtons.forEach((button, index) => {
     paragraphsSections[index].classList.toggle('hidden');
