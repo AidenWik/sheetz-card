c5f2a6c1b98e09596bb052628bb5e6d808fd00d3
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-04 10:10:58 -0400

    s

--- src/sheetz-card.js
+++ src/sheetz-card.js
@@ -160,7 +160,7 @@ class SheetzCard extends LitElement {
 }
 */ 
     const cardContainer = document.querySelector('sheetz-card').shadowRoot.querySelector('.card-container');
-    const cardWrapperClone = document.querySelector('sheetz-card').shadowRoot.querySelector('.card-container').cloneNode(true);
+    const cardWrapperClone = document.querySelector('sheetz-card').shadowRoot.querySelector('.card-wrapper').cloneNode(true);
   
     // Add unique classes to the cloned elements
     
